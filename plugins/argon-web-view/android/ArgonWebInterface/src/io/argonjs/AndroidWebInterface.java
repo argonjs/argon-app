425896a3f58f35c3308cd60d48fe3168f094d83e
Michael Eden <themichaeleden@gmail.com>
2016-04-15 03:07:24 -0400

    removed debug code from web interface jar

--- plugins/argon-web-view/android/ArgonWebInterface/src/io/argonjs/AndroidWebInterface.java
+++ plugins/argon-web-view/android/ArgonWebInterface/src/io/argonjs/AndroidWebInterface.java
@@ -1,18 +1,12 @@
 package io.argonjs;
 
 import android.webkit.JavascriptInterface;
-import android.util.Log;
 
 /**
  * Created by Michael Eden on 4/14/16.
  */
 @SuppressWarnings({"unused", "synthetic-access"})
 public abstract class AndroidWebInterface {
-
-    public void test() {
-        Log.i("[ARGON WEB INTERFACE]", "Called test function.");
-    }
-
     @JavascriptInterface
     public void emit(String event, String json) {
         onArgonEvent(event, json);
