3ab3d955bcdb1833816f2c6d3dab3a3737d3ee57
Taeheon Kim <tkim@mozilla.com>
2017-05-24 11:44:26 -0700

    Allows page reload by clicking submit on the url bar

--- app/main-page.ts
+++ app/main-page.ts
@@ -397,7 +397,8 @@ export function searchBarLoaded(args) {
         searchBar.on(SearchBar.submitEvent, () => {
             let urlString = searchBar.text;
 
-            // if (!urlString) urlString = appViewModel.currentUri;
+            // Allows page reload by clicking submit on the url bar
+            if (urlString == "") urlString = appViewModel.currentUri || "";
 
             if (urlString.includes(" ") || !urlString.includes(".")) {
                 // queries with spaces or single words without dots go to google search
