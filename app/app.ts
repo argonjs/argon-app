f12a2e5ee9736fd9ef93dec86610d30ab604ff39
Kimberly Siva <kspreen@gmail.com>
2017-06-30 09:01:04 -0400

    Attempting to fix url-launch on Android

--- app/app.ts
+++ app/app.ts
@@ -43,6 +43,7 @@ function handleOpenURL(urlString: string) {
 }
 
 import * as analytics from "./components/common/analytics";
+import {setActivityCallbacks, AndroidActivityCallbacks} from "ui/frame";
 if (application.ios) {
     class MyDelegate extends UIResponder implements UIApplicationDelegate {
         public static ObjCProtocols = [UIApplicationDelegate];
@@ -61,13 +62,92 @@ if (application.ios) {
     }
     application.ios.delegate = MyDelegate;
 } else {
+    @JavaProxy("edu.gatech.argon4.MainActivity")
+    class Activity extends android.app.Activity {
+        private _callbacks: AndroidActivityCallbacks;
+
+        protected onCreate(savedInstanceState: android.os.Bundle): void {
+            if (!this._callbacks) {
+                setActivityCallbacks(this);
+            }
+            console.log("*** custom activity onCreate ***");
+            this._callbacks.onCreate(this, savedInstanceState, super.onCreate);
+        }
+
+        protected onSaveInstanceState(outState: android.os.Bundle): void {
+            this._callbacks.onSaveInstanceState(this, outState, super.onSaveInstanceState);
+        }
+
+        protected onStart(): void {
+            console.log("*** custom activity onStart ***");
+            this._callbacks.onStart(this, super.onStart);
+        }
+
+        protected onStop(): void {
+            console.log("*** custom activity onStop ***");
+            this._callbacks.onStop(this, super.onStop);
+        }
+
+        protected onDestroy(): void {
+            this._callbacks.onDestroy(this, super.onDestroy);
+        }
+
+        public onBackPressed(): void {
+            this._callbacks.onBackPressed(this, super.onBackPressed);
+        }
+
+        public onRequestPermissionsResult(requestCode: number, permissions: Array<String>, grantResults: Array<number>): void {
+            console.log("*** custom activity onRequestPermissionsResult ***");
+            this._callbacks.onRequestPermissionsResult(this, requestCode, permissions, grantResults, <any>undefined);
+        }
+
+        protected onActivityResult(requestCode: number, resultCode: number, data: android.content.Intent): void {
+            console.log("*** custom activity onActivityResult ***");
+            this._callbacks.onActivityResult(this, requestCode, resultCode, data, super.onActivityResult);
+        }
+
+        protected onPause(): void {
+            super.onPause();
+            console.log("*** onPause ***");
+            // note that AndroidActivityCallbacks does not handle onPause!
+            // but we need the nativescript application.suspendEvent to fire
+            // TODO: figure out how to fix this
+            // https://discourse.nativescript.org/t/androidactivitycallbacks-onpause-onresume/1811
+        }
+
+        protected onResume(): void {
+            super.onResume();
+            console.log("*** onResume ***");
+            // note that AndroidActivityCallbacks does not handle onResume!
+            // but we need the nativescript application.resumeEvent to fire
+            // TODO: figure out how to fix this
+            // https://discourse.nativescript.org/t/androidactivitycallbacks-onpause-onresume/1811
+        }
+
+        protected onNewIntent(intent: android.content.Intent): void {
+            super.onNewIntent(intent);
+            console.log("*** onNewIntent ***");
+            var extras = intent.getExtras();
+            if (extras) {
+                appViewModel.launchedFromUrl = true;
+                appViewModel.ready.then(()=>{
+                    var url = extras.getString('url');
+                    console.log("*** onNewIntent, url: " + url);
+                    handleOpenURL(url);
+                });
+            }
+        }
+    }
+    console.log("Activity: " + Activity);
+
     application.on(application.launchEvent, function (args) {
+        console.log("*** application.launchEvent ***");
         var extras = args.android.getExtras();
         if (extras) {
-            // TODO: enable url-launch from background state
             appViewModel.launchedFromUrl = true;
             appViewModel.ready.then(()=>{
                 var url = extras.getString('url');
+                console.log("*** launchEvent, url: " + url);
                 handleOpenURL(url);
             });
         }
